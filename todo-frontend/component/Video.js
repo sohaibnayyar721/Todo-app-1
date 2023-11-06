5f7231456c078085ec8e98889ad663014a0b4e35
sohaibnayyar.721 <sohaibnayyar.721@gmail.com>
2023-11-06 20:19:42 +0500

    changes in style

--- todo-frontend/component/Video.js
+++ todo-frontend/component/Video.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Video() {
     return (
         <div className='w-[100%] h-[100vh]'>     
-            <video autoPlay loop muted className="w-[100%] h-full object-cover opacity-50 " >
+            <video autoPlay loop muted className="w-[100%] h-full object-cover opacity-50 md:opacity-25 " >
                 <source src="/bgVideo.mp4" type="video/mp4" />
             </video></div>
     )
