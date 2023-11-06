f7915f5d3a44bd21002e31322e0542ac6ab45ca0
sohaibnayyar.721 <sohaibnayyar.721@gmail.com>
2023-11-06 22:01:17 +0500

    changes in api

--- todo-backend/index.js
+++ todo-backend/index.js
@@ -4,13 +4,13 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 const app=express()
-const corsOptions ={
-    origin:'*', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200,
- }
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://to-do-app-henna-sigma.vercel.app/update/completetask"); // Update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(express.json())
 app.use(router)
 
