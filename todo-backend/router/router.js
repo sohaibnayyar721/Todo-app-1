257d7a1ab9c54d328758f1e3e95922577e33929c
sohaibnayyar.721 <sohaibnayyar.721@gmail.com>
2023-11-07 01:48:44 +0500

    changes in api

--- todo-backend/router/router.js
+++ todo-backend/router/router.js
@@ -7,7 +7,7 @@ router.get('/:getOneTask',getOneTask)
 router.post('/addTask',addTask)
 router.delete('/:deleteTask',deleteTask)
 router.put('/:updateTask',updateTask)
-router.get('/yellow',completeTask)
+router.get('/update/completetask',completeTask)
 router.all('*',(req,res)=>{
     res.send("Cant find any route")
 })
